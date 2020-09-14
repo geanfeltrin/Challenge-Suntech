b844cb0a4b2724c264e56408ebb576ebe6c13950
Gean Feltrin <gean_costa@hotmail.com>
2020-09-14 09:54:22 -0300

    Add Toastfy

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import * as dotenv from 'dotenv';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Routes from './routes';
 import GlobalStyles from './styles/global';
 
+dotenv.config();
 const App: React.FC = () => {
   return (
     <>
       <Router>
         <GlobalStyles />
         <Routes />
+        <ToastContainer autoClose={3000} />
       </Router>
     </>
   );
