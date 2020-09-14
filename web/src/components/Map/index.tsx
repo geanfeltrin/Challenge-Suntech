b08d3e737f5160967130688e1e5ef5ce287ed93b
Gean Feltrin <gean_costa@hotmail.com>
2020-09-14 10:20:51 -0300

    Create Component Map

--- web/src/components/Map/index.tsx
+++ web/src/components/Map/index.tsx
@@ -1,59 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import GoogleMapReact, {
-  Heatmap,
-  Position as PositionProps,
-} from 'google-map-react';
-import api from '../../services/api';
+import React from 'react';
+import GoogleMapReact, { Heatmap } from 'google-map-react';
 
 const center = {
   lat: -3.745,
   lng: -38.523,
 };
 
-interface place {
-  lat: number;
-  lng: number;
-  residents: number;
+interface dataProps {
+  data: Heatmap;
 }
 
-const Map: React.FC = () => {
-  const [data, setData] = useState<PositionProps[]>([]);
-  const [heatmapData, setHeatmapData] = useState<Heatmap>({
-    positions: data,
-    options: {
-      radius: 20,
-      opacity: 1,
-    },
-  });
-
-  useEffect(() => {
-    api.get('/residences').then(response => {
-      const places: place[] = response.data;
-      const residences: PositionProps[] = places.map(position => {
-        return {
-          lat: position.lat,
-          lng: position.lng,
-          weight: position.residents,
-        };
-      });
-
-      setData(residences);
-      setHeatmapData({ ...heatmapData, positions: residences });
-    });
-  }, [heatmapData]);
-
+const Map: React.FC<dataProps> = ({ data }) => {
   return (
-    <div style={{ height: '700px', width: '100%' }}>
-      {data.length >= 1 && (
+    <div style={{ height: '100%', width: '100%' }}>
+      {data.positions.length >= 1 && (
         <GoogleMapReact
           bootstrapURLKeys={{
-            key: 'AIzaSyC3jEhWeNBELtlanSia-L68vsisLYAYVWY',
+            key: process.env.REACT_APP_KEY,
             language: 'pt-br',
-            region: 'BRAZIL',
+            region: 'BR',
             libraries: ['visualization'],
           }}
           defaultCenter={center}
-          heatmap={heatmapData}
+          heatmap={data}
           defaultZoom={5}
         />
       )}
