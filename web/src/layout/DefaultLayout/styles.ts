d39b51a951551b42b2ea8399eb41a99bca3c2345
Gean Feltrin <gean_costa@hotmail.com>
2020-09-14 09:55:15 -0300

    Fix default layout

--- web/src/layout/DefaultLayout/styles.ts
+++ web/src/layout/DefaultLayout/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const WrapperContent = styled.div`
   display: flex;
   width: 100%;
-  height: 100vh;
   max-width: 1350px;
   margin: 0 auto;
   margin-top: 70px;
