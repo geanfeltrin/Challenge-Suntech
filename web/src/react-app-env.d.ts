5423d915ea1dd4a67d305f13210bec031003d1ef
Gean Feltrin <gean_costa@hotmail.com>
2020-09-14 10:20:36 -0300

    Fix env interface

--- web/src/react-app-env.d.ts
+++ web/src/react-app-env.d.ts
@@ -3,9 +3,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
     PUBLIC_URL: string;
-    REACT_APP_HASH: string;
-    REACT_APP_API_URI: string;
-    REACT_APP_WS_URI: string;
+    REACT_APP_KEY: string;
   }
 }
 interface Window {
