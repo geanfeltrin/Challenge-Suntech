990a3544ffbecddfbe43ce10b95345fc6f4de2fa
Gean Feltrin <gean_costa@hotmail.com>
2020-09-15 09:46:16 -0300

    Add responsive layout

--- web/src/layout/DefaultLayout/styles.ts
+++ web/src/layout/DefaultLayout/styles.ts
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 export const WrapperContent = styled.div`
   display: flex;
   width: 100%;
-  max-width: 1350px;
+  max-width: 1650px;
   margin: 0 auto;
   margin-top: 70px;
+  padding: 16px;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+    margin-top: 16px;
+    align-items: center;
+  }
 `;
