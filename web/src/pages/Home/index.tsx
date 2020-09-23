fd865c089d1f8c5c5e7553409951c4e441fb6d9a
Gean Feltrin <gean_costa@hotmail.com>
2020-09-23 08:27:58 -0300

    Remove aria-label

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -40,7 +40,7 @@ const Home: React.FC = () => {
   return (
     <Container>
       <div className="content-title">
-        <h1 aria-label="Mapa de residências">Mapa de residências</h1>
+        <h1>Mapa de residências</h1>
         <small>
           Esse é um mapa de calor informando o local da residência e a
           quantidade de membros.
